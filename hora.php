c10d4993307843b2b4c074d196c3f46799dd43d5
ecr007 <evercuevas1000@gmail.com>
2014-03-21 11:46:13 -0700

    Update hora.php

--- hora.php
+++ hora.php
@@ -1,2 +1,8 @@
-ini_set('date.timezone','America/Santo_Domingo'); 
+ini_set('date.timezone','America/Santo_Domingo'); //Puede que esta no la reconozcan algunos servidores
+
+date_default_timezone_set('America/Santo_Domingo'); //Convierte las horas segun el pais pasado
+
+//Listado de zonas Horarias
+//http://www.php.net/manual/es/timezones.php
+
 echo date("g:i A");
